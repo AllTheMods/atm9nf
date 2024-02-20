62de90ed13f3efd96905a236c6074b34093dd071
Kevin-Marsh <Kevin-Marsh@users.noreply.github.com>
2024-02-20 13:20:21 +1000

    Powah, Botania, and cleanup.
    Powah changes for uraninite and certus energizing, Botania resource duplication, disable everything is copper shield, remove the evoker model from the datapack to stop it incorrectly giving blue hearts.

--- kubejs/server_scripts/mods/endermanoverhaul/tags.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.tags('block', event => {
-    const stones = event.get('minecraft:base_stone_overworld').getObjectIds()
-    stones.forEach(stone => {
-        event.remove('endermanoverhaul:cave_enderman_holdable',stone);
-    })
-})
